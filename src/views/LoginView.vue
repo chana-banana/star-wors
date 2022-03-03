bd662ad6c8790ec388203714d0394b5dc100fc92
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-03 13:21:35 +0200

    feat(login_add): added login view and main css

---
+++ src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="view">
+        <LoginForm />
+    </div>
+</template>
+
+<script>
+    import LoginForm from '../components/LoginForm'
+
+    export default {
+    components: {
+    LoginForm
+  }
+}
+
+</script>
+
+<style scoped>
+  .view {
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    /* background-size: cover; */
+  }
+
+</style>
