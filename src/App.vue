2eb93ff3e9c1c4a1644ff7de366bb81139e90225
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-03 13:26:30 +0200

    chore(login_main-styling): import main styling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <!-- <nav>
-      <router-link to="/login">Login</router-link>
-    </nav> -->
-    <router-view/>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <style>
@@ -13,8 +12,19 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 
-@import './assets/main.css'
+nav {
+  padding: 30px;
+}
 
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -30,6 +30,4 @@
 
 <style scoped>
 
-
-
 </style>
