4d4bd01baa44d0af6a175ea966e1f760bd60cd27
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-03 13:35:49 +0200

    chore(login_link-main-styling): link main styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
 
@@ -15,16 +15,6 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
+@import './assets/main.css'
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
