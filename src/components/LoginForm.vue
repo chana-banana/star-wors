2e99a9c9c3d51796688d1160e11c24193fa50875
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-07 07:53:20 +0200

    feat(show-hide): add show-hide button and icon

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,7 +4,8 @@
           <form>
             <h1>Sign In</h1>
             <input type="email" id="mail" name="user_email" placeholder="Email" v-model="email" />
-            <input type="password" id="password" name="user_password" placeholder="Password" v-model="password" />
+            <input :type=passwordType id="password" name="user_password" placeholder="Password" v-model="password" />
+            <button @click.prevent="toggleShow" class="toggle-password"><fa icon="eye" class="input-icon" /></button>
             <button class="submit" type="submit" @click="signIn">Submit</button>
             <p>Not a member yet? <router-link id="sign-up" to="/register">Sign up here</router-link></p>
           </form>
@@ -17,12 +18,20 @@
     data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      passwordType: 'password'
     }
   },
     methods: {
       signIn() {
         console.log(this.email,this.password)
+      },
+      toggleShow() {
+        if (this.passwordType === 'password') {
+          this.passwordType = 'text'
+        } else {
+          this.passwordType = 'password'
+        }
       }
     }
   }
