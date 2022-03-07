934d2440d7b4c78015bdbba3220a60fd6ac7bfb3
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-07 08:58:13 +0200

    chore(register_show-hide): add show-hide to register page

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,8 +4,10 @@
           <form>
             <h1>Sign In</h1>
             <input type="email" id="mail" name="user_email" placeholder="Email" v-model="email" />
-            <input :type=passwordType id="password" name="user_password" placeholder="Password" v-model="password" />
-            <button @click.prevent="toggleShow" class="toggle-password"><fa icon="eye" class="input-icon" /></button>
+              <div class="buttonIn">
+                <input :type=passwordType id="password" name="user_password" placeholder="Password" v-model="password" />
+                <button @click.prevent="toggleShow" class="toggle-password"><fa icon="eye" class="input-icon" /></button>
+              </div>
             <button class="submit" type="submit" @click="signIn">Submit</button>
             <p>Not a member yet? <router-link id="sign-up" to="/register">Sign up here</router-link></p>
           </form>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -4,8 +4,10 @@
           <form>
             <h1>Sign Up</h1>
             <input type="email" id="mail" name="user_email" placeholder="Email" v-model="email" />
-            <input type="password" id="password" name="user_password" placeholder="Password" v-model="password" />
-            <input type="password" id="password" name="user_password" placeholder="Confirm Password" v-model="password" />
+            <input :type="passwordType" id="password" name="user_password" placeholder="Password" v-model="password" />
+            <button @click.prevent="toggleShow" class="toggle-password"><fa icon="eye" class="input-icon" /></button>
+            <input :type="passwordType" id="confirm-password" name="confirm_password" placeholder="Confirm Password" v-model="confirmPassword" />
+            <button @click.prevent="toggleShow" class="toggle-password"><fa icon="eye" class="input-icon" /></button>
             <router-link id="sign-up" to="/"><button class="submit" type="submit" @click="signIn">Sign Up</button></router-link>
           </form>
         </div>
@@ -17,12 +19,20 @@
     data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      passwordType: 'password'
     }
   },
     methods: {
       signIn() {
         console.log(this.email,this.password)
+      },
+      toggleShow() {
+        if (this.passwordType === 'password') {
+          this.passwordType = 'text'
+        } else {
+          this.passwordType = 'password'
+        }
       }
     }
   }
