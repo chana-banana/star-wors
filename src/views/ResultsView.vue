aeab57f45abe7b3b777ae6a78da4c3b295d5f2d8
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-10 10:43:19 +0200

    feat(character-page): added result view

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <NavBar />
+        <div>
+            <CharacterCard />
+        </div>
+      <FooterComponent />
+    </div>
+</template>
+
+<script>
+import FooterComponent from '../components/FooterComponent'
+import NavBar from '../components/NavBar'
+import CharacterCard from '../components/CharacterCard'
+
+    export default {
+    components: {
+    FooterComponent,
+    NavBar,
+    CharacterCard
+  }
+}
+</script>
