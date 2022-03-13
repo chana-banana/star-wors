b5ef13f8759e15699886836d70f03c79cd2edb25
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-13 19:06:20 +0200

    feat(cart_initiate): add cart item fa-icons

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -3,14 +3,24 @@
         <div>Cart</div>
         <div class="item-wrapper">
             <div class="item-icon"></div>
-            <div class="item-name"></div>
+            <div class="item-name">Luke Skywalker</div>
             <div class="quantity-wrapper">
-                <div class="quantity-icon"></div>
-                <div class="quantity"></div>
-                <div class="quantity-icon"></div>
+                <button class="quantity-icon"><fa icon="minus" class="fa-quantity-icon" /></button>
+                <div class="quantity">02</div>
+                <button class="quantity-icon"><fa icon="plus" class="fa-quantity-icon" /></button>
             </div>
-            <div class="quantity-price"></div>
-            <div class="remove-item"></div>
+            <div class="quantity-price">R200</div>
+            <button class="remove-item"><fa icon="xmark" class="fa-quantity-icon" /></button>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .item-wrapper {
+        display: inline-flex;
+    }
+
+    .quantity-wrapper {
+        display: flex;
+    }
+</style>
