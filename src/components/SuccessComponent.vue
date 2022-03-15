f03bc452d2795f273353c537aa86b368be55ad70
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-15 16:04:11 +0200

    feat(success): added base elements

---
+++ src/components/SuccessComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="r2d2-wrapper">
+          <img src="../assets/icons/success-R2D2-yellow.svg" alt="R2D2" class="r2d2">
+        </div>
+        <div>
+          <p class="hero-text">Success.. We have received the order we were looking for</p>
+          <button class="back-btn"><router-link to="/cart">back to search</router-link></button>
+        </div>
+    </div>
+</template>
+
+<style>
+.r2d2-wrapper {
+    display: block;
+    justify-content: center;
+    align-items: center;
+  }
+
+.back-btn {
+    background-color: #ffe81f;
+    border-radius: 5px;
+    border-color: #ffe81f;
+    color: #000000;
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    font-weight: bold;
+}
+
+.back-btn:hover {
+    background-color: #000000;
+    border-color: #ffe81f;
+    color: #ffe81f;
+}
+</style>
+
---
+++ src/views/SuccessView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <SuccessComponent />
+    </div>
+</template>
+
+<script>
+    import SuccessComponent from '../components/SuccessComponent'
+
+    export default {
+    components: {
+    SuccessComponent
+  }
+}
+
+</script>
