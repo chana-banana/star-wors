4a0d21244262b3233bcb571f010fc10ad61170d9
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-17 20:22:43 +0200

    chore(foot-nav): changed to computed for item count

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,15 +8,9 @@
     </div>
     <div class="nav-right">
       <router-link to=""><p>Order History</p></router-link>
-<<<<<<< HEAD
-      <router-link to="/cart"><img src="../assets/icons/cart-yellow.svg" alt="cart" class="nav-icon"></router-link>
-      <div class="cart-quantity">(0)</div>
-      <router-link to="/login"><img src="../assets/icons/exit-yellow.svg" alt="exit" class="nav-icon"></router-link>
-=======
       <router-link to=""><img src="../assets/icons/cart-yellow.svg" alt="cart" class="nav-icon"></router-link>
       <div class="cart-quantity">({{ countItems }})</div>
       <router-link to=""><img src="../assets/icons/exit-yellow.svg" alt="exit" class="nav-icon"></router-link>
->>>>>>> develop
     </div>
   </nav>
 </template>
