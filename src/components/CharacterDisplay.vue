8633b3ce2ec1724f96b749c39a67c27d0f3ab1fb
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-03-17 20:58:15 +0200

    feat(display-char): basic styling character bio display

--- src/components/CharacterDisplay.vue
+++ src/components/CharacterDisplay.vue
@@ -2,33 +2,109 @@
     <div>
         <div class="bio-wrapper" v-if="person">
             <h4>{{ person.name }}</h4>
-            <div><h5>Birth Year: &nbsp;</h5><h5>{{ person.birth_year }}</h5></div>
-            <div><h5>Hair Color: &nbsp;</h5><h5>{{ person.hair_color }}</h5></div>
-            <div><h5>Skin Color: &nbsp;</h5><h5>{{ person.skin_color }}</h5></div>
-            <div><h5>Eye Color: &nbsp;</h5><h5>{{ person.eye_color }}</h5></div>
-            <div><h5>Gender: &nbsp;</h5><h5>{{ person.gender }}</h5></div>
+            <div class="atribute"><h5>Birth Year: &nbsp;</h5><h5 class="arr-result">{{ person.birth_year }}</h5></div>
+            <div class="atribute"><h5>Hair Color: &nbsp;</h5><h5 class="arr-result">{{ person.hair_color }}</h5></div>
+            <div class="atribute"><h5>Skin Color: &nbsp;</h5><h5 class="arr-result">{{ person.skin_color }}</h5></div>
+            <div class="atribute"><h5>Eye Color: &nbsp;</h5><h5 class="arr-result">{{ person.eye_color }}</h5></div>
+            <div class="atribute"><h5>Gender: &nbsp;</h5><h5 class="arr-result">{{ person.gender }}</h5></div>
+
+            <div class="price-wrapper">
+            <div class="price-text">
+            <div>R100</div>
+            </div>
+            <button @click.stop="addItemToCart(person.name)" class="add-cart">
+            <fa icon="plus" class="fa-plus" />
+            <img src="../assets/icons/cart-yellow.svg" alt="cart" class="cart-yellow">
+            <img src="../assets/icons/cart-black.svg" alt="cart" class="cart-black">
+            </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            person: Object
-        }
+export default {
+    props: {
+        person: Object
+    },
+    methods: {
+    addItemToCart(name) {
+        this.$store.dispatch('addItemToCart', name)
     }
+}
+}
 </script>
 
 <style scoped>
 .bio-wrapper {
-    width: 243px;
+    width: 278px;
     min-height: 344px;
     border-style: solid;
     border-color: #ffe81f;
-    /* border-radius: 13px; */
     border-width: 2px;
-    margin: 1rem 3rem;
+    margin: auto;
     padding: 1rem;
     position: relative;
+}
+
+.price-wrapper {
+    height: 2.125em;
+    border-radius: 0.3125em;
+    width: calc(100% - 2rem);
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    bottom: 1rem;
+}
+
+.price-text {
+    background-color: #ffe81f;
+    color: #000000;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    padding: 0.5rem;
+}
+.add-cart {
+    background-color: #000000;
+    border-radius: 5px;
+    border-color: #ffe81f;
+    color: #ffe81f;
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
+
+.add-cart:hover {
+    background-color: #ffe81f;
+}
+
+.add-cart:hover .cart-yellow {
+    display: none;
+}
+
+.add-cart:hover .cart-black, .add-cart:hover .fa-plus {
+    display: inline-block;
+    color: #000000;
+}
+
+.cart-black {
+    display: none;
+}
+
+.cart-yellow, .cart-black {
+    height: 18px;
+    margin: 5px;
+}
+
+.atribute {
+    display: flex;
+    padding: 0.75rem 0;
+}
+
+.arr-result {
+    font-weight: 100;
 }
 </style>
