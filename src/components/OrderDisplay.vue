aafb0c4176032d9d1b752ad5650c2267cb0942ab
Chanlé van der Merwe <chanlevdmerwe@gmail.com>
2022-04-05 10:58:56 +0200

    feat(order-view): added props to order display

--- src/components/OrderDisplay.vue
+++ src/components/OrderDisplay.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <div>{{ order.OrderNumber }}</div>
       <div class="item-wrapper" v-if="order" >
+        <div>{{ order.OrderNumber }}</div>
         <div>{{ order.id }}</div>
         <div>{{ order.count }}</div>
         <div>{{ order.price }}</div>
+        <div>{{ order.totalCartAmount }}</div>
       </div>
-    <div>{{ order.totalCartAmount }}</div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    order: Object
+  }
+}
+
+</script>
